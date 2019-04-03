9c8a7e4f8002a3ad04b707124e0d916b72e27031
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-04-03 14:59:07 +0200

    3.1.1 IntArrayStack class

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,52 +1,5 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-    private static final int DEFAULT_RETURN_VALUE = -1;
-    private static final int MAX_STACK_SIZE = 12;
-    private int[] numbers = new int[getMaxStackSize()];
-
-    private int total = getDefaultReturnValue();
-
-    public static int getDefaultReturnValue() {
-        return DEFAULT_RETURN_VALUE;
-    }
-
-    public static int getMaxStackSize() {
-        return MAX_STACK_SIZE;
-    }
-
-    protected void countIn(int in) {
-        if (!isFull())
-            getNumbers()[++total] = in;
-    }
-
-    protected boolean callCheck() {
-        return total == getDefaultReturnValue();
-    }
-
-    protected boolean isFull() {
-        return total == getMaxStackSize() -1;
-    }
-
-    public int peekaboo() {
-        if (callCheck())
-            return getDefaultReturnValue();
-        return getNumbers()[total];
-    }
-
-    public int countOut() {
-        if (callCheck())
-            return getDefaultReturnValue();
-        return getNumbers()[total--];
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-
-    public int[] getNumbers() {
-        return numbers;
-    }
 
 }
---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,51 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private static final int DEFAULT_RETURN_VALUE = -1;
+    private static final int MAX_STACK_SIZE = 12;
+    private int[] numbers = new int[getMaxStackSize()];
+    private int total = getDefaultReturnValue();
+
+    public static int getDefaultReturnValue() {
+        return DEFAULT_RETURN_VALUE;
+    }
+
+    public static int getMaxStackSize() {
+        return MAX_STACK_SIZE;
+    }
+
+    protected boolean callCheck() {
+        return total == getDefaultReturnValue();
+    }
+
+    protected boolean isFull() {
+        return total == getMaxStackSize() -1;
+    }
+
+    protected void countIn(int in) {
+        if (!isFull())
+            getNumbers()[++total] = in;
+    }
+
+
+    public int peekaboo() {
+        if (callCheck())
+            return getDefaultReturnValue();
+        return getNumbers()[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return getDefaultReturnValue();
+        return getNumbers()[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+}
